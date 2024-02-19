f9aa731312f530ab3cd85119ad0a57b555a781ad
kindddddd <kinddeath1@gmail.com>
2024-02-20 02:10:48 +0300

    hot fix

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,17 @@
 function checkLength (string, maxLength) {
-  result = (string.length <= maxLength) ? true : false;
-  return result
+  let result = (string.length <= maxLength) ? true : false;
+  return result;
 }
 
 function checkIfPalindrom (string) {
-    let noSpaceString = string.replaceAll(' ','');
-    let normalisedString = noSpaceString.toUpperCase();
-    let reversedString = '';
-    for (let i = normalisedString.length - 1; i >= 0; i--) {
+  const noSpaceString = string.replaceAll(' ','');
+  const normalisedString = noSpaceString.toUpperCase();
+  let reversedString = '';
+  for (let i = normalisedString.length - 1; i >= 0; i--) {
         reversedString += normalisedString[i];
     }
 
-    let isPalindrom = (reversedString === normalisedString);
+  let isPalindrom = (reversedString === normalisedString);
 
     return isPalindrom;
 }
