3d527c9f6264609431913617944ee4bc7780a3a9
kindddddd <kinddeath1@gmail.com>
2024-04-07 03:26:36 +0300

    big rafctoring

--- js/createArrayMiniatures.js
+++ js/createArrayMiniatures.js
@@ -5,24 +5,51 @@ import { getDataArrayMiniatures } from "./data.js";
 const OBJECT_COUNT = 25;
 const { DESCRIPTIONS, COMMENTS, NAMES } = getDataArrayMiniatures();
 
-const getPhotoId = createRandomIdFromRangeGenerator(1, 26);
-const generateCommentId = createRandomIdFromRangeGenerator(1, 26);
-const getNumberPhoto = createRandomIdFromRangeGenerator(1, 26);
+const initId = () => {
+  let id = 0;
+  // функция получения  Id
+  return function () {
+    id++;
+    return id;
+  };
+};
 
-const createComments = () =>({
-  id:generateCommentId(),
-  avatar: `img/avatar-${getRandomInteger(1,6)}.svg`,
-  message: getRandomArrayElement(COMMENTS),
-  name: getRandomArrayElement(NAMES),
-});
 
-const createPhotoDescription = () => ({
-  id: getPhotoId(),
-  url: `photos/${getNumberPhoto()}.jpg`,
-  description: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomInteger(15, 200),
-  comments: Array.from({length: getRandomInteger(0, 30) }, createComments),
-});
+const getNextCommentId = initId();
+const getNextPhotoId = initId();
+
+
+const createComments = () => {
+  const id = getNextCommentId();
+  const avatar = `img/avatar-${getRandomInteger(1, 6)}.svg`;
+  const message = getRandomArrayElement(COMMENTS);
+  const name = getRandomArrayElement(NAMES);
+
+  return {
+    id,
+    avatar,
+    message,
+    name
+  };
+};
+
+// функция, которая будет создавать объект с описанием фотографии
+const createPhotoDescription = () => {
+  const id = getNextPhotoId();
+  const url = `photos/${id}.jpg`;
+  const description = getRandomArrayElement(DESCRIPTIONS);
+  const likes = getRandomInteger(15, 200);
+  const comments = Array.from({ length: getRandomInteger(0, 30) }, createComments);
+
+  return {
+    id,
+    url,
+    description,
+    likes,
+    comments
+  };
+};
+
 
 const getArrayMiniatures = () =>
   Array.from({ length: OBJECT_COUNT}, createPhotoDescription);
