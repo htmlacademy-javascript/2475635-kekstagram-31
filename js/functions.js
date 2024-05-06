b13ef981a711ef2a50bf2a15a076db413e08a408
kindddddd <kinddeath1@gmail.com>
2024-05-06 22:41:05 +0300

    1 more fix

--- js/functions.js
+++ js/functions.js
@@ -14,20 +14,20 @@ function checkIfPalindrom (string) {
   return isPalindrom;
 }
 
-  const transformTimeToNumber = (timeString) => {
-    const timeParts = timeString.split(':');
-    const hours = parseInt(timeParts[0], 10);
-    const minutes = parseInt(timeParts[1], 10);
-    return hours * 60 + minutes;
-  };
+const transformTimeToNumber = (timeString) => {
+  const timeParts = timeString.split(':');
+  const hours = parseInt(timeParts[0], 10);
+  const minutes = parseInt(timeParts[1], 10);
+   return hours * 60 + minutes;
+};
 
-  const checkEnoughTime = (startDay, endDay, startMeeting, meetingDuration) => {
-    const startDayInMinutes = makeTimeToNumber(startDay);
-    const endDayInMinutes = makeTimeToNumber(endDay);
-    const startMeetingInMinutes = makeTimeToNumber(startMeeting);
-    const CheckIfIsEnoughTime = (startDay, endDay, startMeeting, meetingDuration) => {
-      const startDayInMinutes = transformTimeToNumber(startDay);
-      const endDayInMinutes = transformTimeToNumber(endDay);
-      const startMeetingInMinutes = transformTimeToNumber(startMeeting);
-      return startMeetingInMinutes >= startDayInMinutes && (endDayInMinutes - startMeetingInMinutes) >= meetingDuration;
-    };};
+const checkEnoughTime = (startDay, endDay, startMeeting, meetingDuration) => {
+  const startDayInMinutes = makeTimeToNumber(startDay);
+  const endDayInMinutes = makeTimeToNumber(endDay);
+  const startMeetingInMinutes = makeTimeToNumber(startMeeting);
+  const CheckIfIsEnoughTime = (startDay, endDay, startMeeting, meetingDuration) => {
+    const startDayInMinutes = transformTimeToNumber(startDay);
+    const endDayInMinutes = transformTimeToNumber(endDay);
+    const startMeetingInMinutes = transformTimeToNumber(startMeeting);
+    return startMeetingInMinutes >= startDayInMinutes && (endDayInMinutes - startMeetingInMinutes) >= meetingDuration;
+  };};
